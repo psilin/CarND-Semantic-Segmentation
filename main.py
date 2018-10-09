e4e27ac70ed408e08bc97977aec0d8c86494f861
Pavel Silin <psilin@topcon.com>
2018-10-09 22:11:24 +0300

    nn architecture & test fix

--- main.py
+++ main.py
@@ -33,8 +33,15 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    image_input = tf.get_default_graph().get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = tf.get_default_graph().get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3_out = tf.get_default_graph().get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4_out = tf.get_default_graph().get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7_out = tf.get_default_graph().get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    return image_input, keep_prob, layer3_out, layer4_out, layer7_out
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -48,7 +55,46 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :return: The Tensor for the last layer of output
     """
     # TODO: Implement function
-    return None
+    w_init_stddev = 0.01
+    w_reg_l2 = 0.001
+
+    # 1x1 convolution of vgg layer 7
+    conv_1x1_of_l7 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, padding='same',
+                                     kernel_initializer=tf.random_normal_initializer(stddev=w_init_stddev),
+                                     kernel_regularizer=tf.contrib.layers.l2_regularizer(w_reg_l2))
+
+    # Upsample deconvolution X 2
+    conv_1x1_of_l7_upsampleX2 = tf.layers.conv2d_transpose(conv_1x1_of_l7, num_classes, 4, strides=(2, 2), padding='same',
+                                                  kernel_initializer=tf.random_normal_initializer(stddev=w_init_stddev),
+                                                  kernel_regularizer=tf.contrib.layers.l2_regularizer(w_reg_l2))
+
+    # 1x1 convolution of vgg layer 4
+    conv_1x1_of_l4 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, padding ='same',
+                                     kernel_initializer=tf.random_normal_initializer(stddev=w_init_stddev),
+                                     kernel_regularizer=tf.contrib.layers.l2_regularizer(w_reg_l2))
+
+    # Skip layer
+    skip1 = tf.add(conv_1x1_of_l7_upsampleX2, conv_1x1_of_l4)
+
+    # Upsample deconvolutions X 2
+    skip1_upsampleX2 = tf.layers.conv2d_transpose(skip1, num_classes, 4, strides=(2, 2), padding='same',
+                                                   kernel_initializer=tf.random_normal_initializer(stddev=w_init_stddev),
+                                                   kernel_regularizer=tf.contrib.layers.l2_regularizer(w_reg_l2))
+
+    # 1x1 convolution of vgg layer 3
+    conv_1x1_of_l3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, padding ='same',
+                                     kernel_initializer=tf.random_normal_initializer(stddev=w_init_stddev),
+                                     kernel_regularizer=tf.contrib.layers.l2_regularizer(w_reg_l2))
+
+    # Skip layer
+    skip2 = tf.add(skip1_upsampleX2, conv_1x1_of_l3)
+
+    # Upsample deconvolution X 8
+    skip2_upsampleX8 = tf.layers.conv2d_transpose(skip2, num_classes, 16, strides=(8, 8), padding='same',
+                                                  kernel_initializer=tf.random_normal_initializer(stddev=w_init_stddev),
+                                                  kernel_regularizer=tf.contrib.layers.l2_regularizer(w_reg_l2))
+
+    return skip2_upsampleX8
 tests.test_layers(layers)
 
 
@@ -59,15 +105,32 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param correct_label: TF Placeholder for the correct label image
     :param learning_rate: TF Placeholder for the learning rate
     :param num_classes: Number of classes to classify
-    :return: Tuple of (logits, train_op, cross_entropy_loss)
+    :return: Tuple of (logits, train_op, cross_entropy_loss, iou, iou_op)
     """
+
     # TODO: Implement function
-    return None, None, None
+    #IOU
+    prediction = tf.argmax(nn_last_layer, axis=3)
+    ground_truth = tf.argmax(correct_label, axis=3)
+    iou, iou_op = tf.metrics.mean_iou(prediction, ground_truth, num_classes)
+
+    # Logits is a 2D tensor where each row corresponds to a pixel and each column corresponds to a class
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    correct_label = tf.reshape(correct_label, (-1,num_classes))
+
+    # Loss function
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=correct_label))
+
+    # define training operation, chose Adam optimizer for vsriable lerning rate
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
+    train_op = optimizer.minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss, iou, iou_op
 tests.test_optimize(optimize)
 
 
 def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
-             correct_label, keep_prob, learning_rate):
+             correct_label, keep_prob, learning_rate, iou, iou_op):
     """
     Train neural network and print out the loss during training.
     :param sess: TF Session
@@ -80,9 +143,35 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param correct_label: TF Placeholder for label images
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
+    :param iou TP Placeholder intersection-ver-union
+    :param iou_op TF operation to compute IOU
     """
     # TODO: Implement function
-    pass
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
+
+    print("Training started.")
+    print()
+    for i in range(epochs):
+
+        print("Epoch {} ...".format(i + 1))
+        for image, label in get_batches_fn(batch_size):
+            _, loss = sess.run([train_op, cross_entropy_loss],
+                               feed_dict={input_image: image,
+                                          correct_label: label,
+                                          keep_prob: 0.5,
+                                          learning_rate: 0.0001})
+            print("Loss: = {:.3f}".format(loss))
+
+            sess.run(iou_op, feed_dict={input_image: image,
+                                        correct_label: label,
+                                        keep_prob: 1.0})
+
+        avg_iou = sess.run(iou)
+        print("Epoch's IOU: = {:.3f}".format(avg_iou))
+        print()
+    print("Training finished.")
+    print()
 tests.test_train_nn(train_nn)
 
 
@@ -110,11 +199,24 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
+        epochs = 30
+        batch_size = 2
+
+        correct_label = tf.placeholder(tf.int32, [None, None, None, num_classes], name='correct_label')
+        learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+
+        input_image, keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out = load_vgg(sess, vgg_path)
+
+        nn_last_layer = layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
+
+        logits, train_op, cross_entropy_loss, iou, iou_op = optimize(nn_last_layer, correct_label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, 
+                 input_image, correct_label, keep_prob, learning_rate, iou, iou_op)
 
         # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
 
--- project_tests.py
+++ project_tests.py
@@ -98,7 +98,7 @@ def test_optimize(optimize):
     layers_output = tf.Variable(tf.zeros(shape))
     correct_label = tf.placeholder(tf.float32, [None, None, None, num_classes])
     learning_rate = tf.placeholder(tf.float32)
-    logits, train_op, cross_entropy_loss = optimize(layers_output, correct_label, learning_rate, num_classes)
+    logits, train_op, cross_entropy_loss, iou, iou_op = optimize(layers_output, correct_label, learning_rate, num_classes)
 
     _assert_tensor_shape(logits, [2*3*4, num_classes], 'Logits')
 
@@ -125,6 +125,8 @@ def test_train_nn(train_nn):
     correct_label = tf.placeholder(tf.float32, name='correct_label')
     keep_prob = tf.placeholder(tf.float32, name='keep_prob')
     learning_rate = tf.placeholder(tf.float32, name='learning_rate')
+    iou =  tf.constant(0.)
+    iou_op = tf.constant(0)
     with tf.Session() as sess:
         parameters = {
             'sess': sess,
@@ -136,7 +138,9 @@ def test_train_nn(train_nn):
             'input_image': input_image,
             'correct_label': correct_label,
             'keep_prob': keep_prob,
-            'learning_rate': learning_rate}
+            'learning_rate': learning_rate,
+            'iou': iou,
+            'iou_op': iou_op}
         _prevent_print(train_nn, parameters)
 
 
